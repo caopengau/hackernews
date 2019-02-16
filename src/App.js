67b2ee9b56242a6e827709f23c96f7fc8a81f4fd
CaoPeng <caopengau@gmail.com>
2019-02-16 16:20:30 +1100

    functional components, css

--- src/App.js
+++ src/App.js
@@ -43,7 +43,9 @@ class App extends Component {
   }
 
   onDismiss(id) { 
-    const isNotId = item => item.objectID !== id; const updatedList = this.state.list.filter(isNotId); this.setState({ list: updatedList })
+    const isNotId = item => item.objectID !== id; 
+    const updatedList = this.state.list.filter(isNotId); 
+    this.setState({ list: updatedList })
   }
 
 
@@ -53,12 +55,15 @@ class App extends Component {
     const { searchTerm, list } = this.state; 
 
     return (
-      <div className="App">
+      <div className="page">
+        <div className="interactions">
         <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}>
-          Search
-        </Search>
+            value={searchTerm}
+            onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
+
         <Table
           list={list}
           pattern={searchTerm}
@@ -71,47 +76,101 @@ class App extends Component {
   } 
 }
 
-class Search extends Component{
-  render(){
-    const {value, onChange, children} = this.props;
-    return (
-      <form>
-        {children}
-        <input 
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    )
-  }
+// lightweight functional stateless component
+const Search = ({value,onChange,children})=>
+    <form>
+      {children}
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      >
+      </input>
+    </form>
+
+// lightweight functional stateless component with block body
+// function Search(props){
+//   const{value,onChange,children} = props;
+//   return(
+//     <form>
+//       {children}
+//       <input
+//         type="text"
+//         value={value}
+//         onChange={onChange}
+//       >
+//       </input>
+//     </form>
+//   )
+// }
+
+// class Search extends Component{
+//   render(){
+//     const {value, onChange, children} = this.props;
+//     return (
+//       <form>
+//         {children}
+//         <input 
+//           type="text"
+//           value={value}
+//           onChange={onChange}
+//         />
+//       </form>
+//     )
+//   }
+// }
+
+function Table(props){
+  const {pattern, onDismiss} = props;
+  return(
+    <div className="table">
+      {list.filter(isSearched(pattern)).map(item=>
+       <div key={item.objectID} className="table-row">
+          <span style={{ width: '40%' }}> <a href={item.url}>{item.title}</a> </span> 
+          <span style={{ width: '30%' }}> {item.author} </span> 
+          <span style={{ width: '10%' }}> {item.num_comments} </span> 
+          <span style={{ width: '10%' }}> {item.points} </span> 
+          <span style={{ width: '10%' }}> <Button onClick={() => onDismiss(item.objectID)} className="button-inline" > Dismiss </Button></span>
+       </div>
+        )}
+    </div>
+  )
 }
 
-class Table extends Component{
-  render(){
-    const {list, pattern, onDismiss} = this.props;
-    return(
-      <div>
-        {list.filter(isSearched(pattern)).map(item=>
-         <div key={item.objectID}>
-          <span><a href={item.url}>{item.title}</a></span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <Button
-              onClick={()=>onDismiss(item.objectID)}
-            >
-              Dismiss
-            </Button>
-          </span>
-         </div>
-          )}
-      </div>
-    )
-  }
-}
-
+// class Table extends Component{
+//   render(){
+//     const {list, pattern, onDismiss} = this.props;
+//     return(
+//       <div>
+//         {list.filter(isSearched(pattern)).map(item=>
+//          <div key={item.objectID}>
+//           <span><a href={item.url}>{item.title}</a></span>
+//           <span>{item.author}</span>
+//           <span>{item.num_comments}</span>
+//           <span>{item.points}</span>
+//           <span>
+//             <Button
+//               onClick={()=>onDismiss(item.objectID)}
+//             >
+//               Dismiss
+//             </Button>
+//           </span>
+//          </div>
+//           )}
+//       </div>
+//     )
+//   }
+// }
+// function Button(props){
+//   const {onClick,className,children} = props;
+//   return(
+//     <button
+//       onClick={onClick}
+//       className={className}
+//       type="button"
+//     >{children}</button>
+//   )
+// }
 class Button extends Component{
   render(){
     const{
